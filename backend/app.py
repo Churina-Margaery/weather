719828d14e4e877cd8ed5954df2df4fa94e07c32
kristina <kristina.bykova.03@list.ru>
2025-04-13 19:17:19 +0300

    methods get + cors

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,35 @@
-from flask import Flask, render_template, request
-from weather import main as get_weather
+from flask import Flask, jsonify, request
+from weather import current_weather, forecast_weather
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    data = None
-    error = None
-    if request.method == 'POST':
-        location = request.form['cityName']
-        parts = location.split(',')
-        if len(parts) == 1:
-            city = parts[0].strip()
-            region = None
-        else:
-            city = parts[0].strip()
-            region = parts[1].strip()
-        result, status_code = get_weather(city, region)
-        if status == 200:
-            data = result
-        else:
-            error = result['error']
+@app.route('/', methods=['GET'])
+def get_current_weather():
+    city_name = request.args.get('city_name')  
+    state_name = request.args.get('state_name', None)  
+    country_name = request.args.get('country_name') 
 
-    return render_template('index.html', data=data, error=error)
+    current, status_code = current_weather(city_name, state_name, country_name)
+
+    if status_code == 200:
+        return jsonify(current), 200
+    else:
+        return jsonify({"detail": "Failed to get weather data"}), status_code
+
+@app.route('/forecast', methods=['GET'])
+def get_forecast_weather():
+    city_name = request.args.get('city_name')  
+    state_name = request.args.get('state_name', None) 
+    country_name = request.args.get('country_name')
+
+    forecast, status_code = forecast_weather(city_name, state_name, country_name)
+
+    if status_code == 200:
+        return jsonify(forecast), 200  
+    else:
+        return jsonify({"detail": "Failed to get weather data"}), status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
