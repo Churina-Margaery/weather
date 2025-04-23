d3d2629ba3979243ebd1a0f0ab63f4b9634ebad5
Churina Margaery <elshanskaya.r@inbox.ru>
2025-04-23 19:54:32 +0300

    lints fixed

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -18,11 +18,7 @@ function App(): JSX.Element {
   const isLoading = useAppSelector(getIsLoading);
 
   useEffect(() => {
-    try {
-      dispatch(fetchWeatherAction({ cityName: 'Saint-Petersburg', stateName: '', countryName: '' }));
-    } catch (err) {
-      console.log(3, err);
-    }
+    dispatch(fetchWeatherAction({ cityName: 'Saint-Petersburg', stateName: '', countryName: '' }));
     const systemDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (systemDarkMode) {
       dispatch(toggleTheme());
