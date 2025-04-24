a26e706fd663c7576996e58188aa7dc8bb0776ed
sol <sasha.kudryavtsev.20@gmail.com>
2025-04-24 22:13:49 +0300

    Finish tests

--- ui_test/src/test/java/com/example/test/WeatherPageTest.java
+++ ui_test/src/test/java/com/example/test/WeatherPageTest.java
@@ -3,10 +3,11 @@ package com.example.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import com.example.elements.AdditionalInfoBlock;
 import com.example.elements.MainInfoBlock;
+import com.example.elements.NextDayWeatherBlock;
 import com.example.elements.SearchString;
+import com.example.elements.StatisticBlock;
 import com.example.pages.GitHubPage;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.qameta.allure.Allure;
@@ -73,25 +74,66 @@ public class WeatherPageTest extends BaseTest {
 
     @Test
     @DisplayName("Проверка блока с информацией о погоде на следующий день")
-    public void checkNextDayInformation() {
+    public void checkNextHoursInformation() {
+        NextDayWeatherBlock nextDayWeatherBlock = new NextDayWeatherBlock();
 
+        Allure.step("Проверяем отображение погоды в следующие несколько часов", () -> {
+            nextDayWeatherBlock.checkDaysList("Tuesday", "Wednesday", "Wednesday", "Wednesday", "Wednesday");
+            nextDayWeatherBlock.checkTimeList("21:00", "00:00", "03:00", "06:00", "09:00");
+            nextDayWeatherBlock.checkTemperatureList("10 °C", "9 °C", "8 °C", "7 °C", "6 °C");
+        });
     }
 
     @Test
     @DisplayName("Проверка отображения блока со статистикой")
     public void checkStatisticBlock() {
-        
+
+        StatisticBlock statisticBlock = new StatisticBlock();
+
+        Allure.step("Проверка отображения блока статистика", () -> {
+            statisticBlock.openSelect(true);
+            statisticBlock.checkCurrentOption(true, "Temperature");
+            statisticBlock.checkOptions(true);
+
+            statisticBlock.openSelect(false);
+            statisticBlock.checkCurrentOption(false, "Temperature");
+            statisticBlock.checkOptions(false);
+        });
     }
 
     @Test
     @DisplayName("Изменение параметра в статистике за три дня")
     public void changeParametrForThreeDays() {
         
+        StatisticBlock statisticBlock = new StatisticBlock();
+
+        Allure.step("Открываем список опций и проверяем его", () -> {
+            statisticBlock.openSelect(true);
+            statisticBlock.checkCurrentOption(true, "Temperature");
+            statisticBlock.checkOptions(true);
+        });
+
+        Allure.step("Выбираем новую опцию", () -> {
+            statisticBlock.chooseOption("Pressure", true);
+            statisticBlock.checkCurrentOption(true, "Pressure");
+        });
     }
 
     @Test
     @DisplayName("Изменение параметра в статистике за 10 дней")
     public void changeParametrForTenDays() {
         
+        StatisticBlock statisticBlock = new StatisticBlock();
+
+        Allure.step("Открываем список опций и проверяем его", () -> {
+            statisticBlock.openSelect(false);
+            statisticBlock.checkCurrentOption(false, "Temperature");
+            statisticBlock.checkOptions(false);
+        });
+
+        Allure.step("Выбираем новую опцию и проверяем ее", () -> {
+            statisticBlock.chooseOption("Humidity", false);
+            statisticBlock.checkCurrentOption(false, "Humidity");
+        });
     }
 }
--- ui_test/src/test/java/com/example/utils/DateUtil.java
+++ ui_test/src/test/java/com/example/utils/DateUtil.java
@@ -2,7 +2,6 @@ package com.example.utils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class DateUtil {
@@ -13,10 +12,5 @@ public class DateUtil {
         String formattedDate = today.format(formatter);
 
         return formattedDate;
-    }
-
-    public static String getNextDayOfWeek() {
-        LocalDate nextDay = LocalDate.now().plusDays(1);
-        return nextDay.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
     }
 }
