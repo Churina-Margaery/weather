3ae8af726f84ec0faa43cbd91043c63b3576cd2d
sol <sasha.kudryavtsev.20@gmail.com>
2026-03-08 19:38:41 +0300

    microfix

--- ui_test/src/test/java/com/example/elements/NextDayWeatherBlock.java
+++ ui_test/src/test/java/com/example/elements/NextDayWeatherBlock.java
@@ -68,7 +68,7 @@ public class NextDayWeatherBlock implements LoadablePage {
     }
 
     public void checkTimeList() {
-        String timePattern = "([0-1]?[0-9]|2[0-3]):[0-5][0-9]";
+        String timePattern = "^([0-1]?[0-9]|2[0-3]|24):[0-5][0-9]$";
     
         String time1 = $(firstTemperatureTime).getText();
         String time2 = $(secondTemperatureTime).getText();
